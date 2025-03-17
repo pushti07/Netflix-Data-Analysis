e785f370416603f6e7888ad16303854c1d0d5dcd
Pushti Maheshwari <144027532+pushti07@users.noreply.github.com>
2025-03-17 21:13:23 +0530

    Update MovieDataAnalysis.py

--- MovieDataAnalysis.py
+++ MovieDataAnalysis.py
@@ -67,3 +67,45 @@ df['Genre'].dtypes
 df.nunique()
 
 
+#visualizing genre column
+sns.catplot(y = 'Genre', data = df, kind = 'count', order = df['Genre' ].value_counts().index, color = '#4287f5')
+plt.title('genre column distribution' )
+plt.show()
+
+
+# visualizing vote_average column
+sns.catplot(y = 'Vote_Average', data = df, kind = 'count', order = df['Vote_Average' ].value_counts().index, color = '#4287f5')
+plt.title('votes destribution' )
+plt.show()
+
+
+df[df['Popularity'] == df['Popularity'].max()]
+
+
+df[df['Popularity'] == df['Popularity'].min()]
+
+
+df['Release_Date'].hist()
+plt.title('Release_Date column distribution')
+plt.show()
+
+
+### Q1: What is the most frequent genre in the dataset?
+
+#### Drama genre is the most frequent genre in our dataset and has appeared more than 14% of the times among 19 other genres.
+
+### Q2: What genres has highest votes ?
+
+#### we have 25.5% of our dataset with popular vote (6520 rows). Drama again gets the highest popularity among fans by being having more than 18.5% of movies popularities.
+
+### Q3: What movie got the highest popularity ? what's its genre ?
+
+#### Spider-Man: No Way Home has the highest popularity rate in our dataset and it has genres of Action, Adventure and Sience Fiction.
+
+### Q3: What movie got the lowest popularity ? what's its genre ?
+
+#### The united states, thread' has the highest lowest rate in our dataset and it has genres of music , drama , 'war', 'sci-fi' and history'.
+
+### Q4: Which year has the most filmmed movies?
+
+#### year 2020 has the highest filmming rate in our dataset.
